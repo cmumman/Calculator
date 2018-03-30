acadb7be7279b9c6980cb8aadec7fa05e1027dc0
cmumman <cmumman@jci.com>
2018-03-30 12:37:09 +0530

    adding to divison function

---
+++ calculator/Functions/list.py
@@ -0,0 +1,15 @@
+test_list = [ 1,2,"hi"]
+
+print test_list[1]
+
+test_list[1] = "hi"
+
+print test_list
+
+test_list.append("hello")
+
+test_list.extend(["vardhan", "reddy"])
+
+#test_list.insert("rename")
+
+print test_list
