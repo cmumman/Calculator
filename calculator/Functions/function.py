492fa88e26165edc781b7c386eaac73a86112206
cmumman <cmumman@jci.com>
2018-04-12 19:08:25 +0530

    adding  classes

--- calculator/Functions/function.py
+++ calculator/Functions/function.py
@@ -1,4 +1,4 @@
-def pizza():
+'''def pizza():
     """this functions refers ingrediants of pizza"""
     recip = ['bread','onion']
     print recip
@@ -19,4 +19,26 @@ def add_1(x,y):
 
 sum_1 = add_1(3, "a")
 
-print sum_1
+print sum_1 '''
+
+def fab():
+    a,b = 1,1
+    while 1:
+        yield a
+        print a,b
+        a , b = b,a+b
+
+cnt = 0
+for n in fab():
+    print n
+    cnt += 1
+    if cnt == 10:
+        break
+
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = []
+
+for n in numbers:
+    if n > 0:
+     newlist.append(n)
+print(newlist)
